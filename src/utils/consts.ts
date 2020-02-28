fd4069ba0c1c80289bd212b0a602b1d799b4ea74
Andrey Grinblat <expandrey@mail.ru>
2020-02-29 02:46:58 +0300

    master Добавлены параметры

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,19 +1,21 @@
-import { IFractal, Motion } from './types';
+import { IFractal } from './types';
 
 /**
  * Дефолтные настройки параметров фрактала.
  */
 export const defaultParams: IFractal = {
-            name: 2,
+            name: "2",
+            xCenter: 0,
+            yCenter: 0,
             scale: [3.0, 3.0, 0.0, 0.0],
             scaleMode: 0,
             scaleMotion: 0,
             speedScaleMotion: 1.0,
             norm: 'Abs',
-            speedNorm: 1.0000000002,
-            motion: Motion.stop,
-            speedMotion: 1.000000002,
+            speedNorm: 1,
+            motion: 'stop',
+            speedMotion: 1,
             colorStyle: 'gamma17',
-            speedColorStyle: 1.000000002,
+            speedColorStyle: 1,
             animated: false,
         };
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,20 +1,32 @@
 /**
  * Тип объекта фрактала
  */
- export enum Motion {stop, move};
  
  export interface IFractal {
-     name: string | number;
+     name?: string;
+     xCenter?: number;
+     yCenter?: number;
      scale: [number, number, number, number];
      scaleMode: number;
      scaleMotion: number;
      speedScaleMotion: number;
-     norm: string;
-     speedNorm: number;
-     motion: Motion;
-     speedMotion: number;
-     colorStyle: string;
-     speedColorStyle: number;
-     animated: boolean;
+     norm?: string;
+     speedNorm?: number;
+     motion?: string,
+     speedMotion?: number;
+     colorStyle?: string;
+     speedColorStyle?: number;
+     animated?: boolean;
+     changeName?(): void;
+     changeNorm?(): void;
+     changeColor?(): void;
+     changeMotion?(): void;
+     changeSpeedMotion?(): void;
+     changeSpeedNorm?(): void;
+     changeSpeedColorStyle?(): void;
+     changeXCenter?(): void;
+     changeYCenter?(): void;
+     toggleAnimated?(): void;
+     clickApplyButton?(name: string | undefined, norm: string | undefined, colorStyle: string | undefined, motion: string | undefined): void;
  };
  
